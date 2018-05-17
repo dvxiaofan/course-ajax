78b54d83e1ee5281a6c057fa8d8209bf6f3b7ee8
xiaofan <1353815633@qq.com>
2018-05-17 16:13:01 +0800

    request-jQuery

--- async-w-jQuery/app.js
+++ async-w-jQuery/app.js
@@ -9,27 +9,36 @@
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
         // 添加图片
-        var imgRequest = new XMLHttpRequest();
-        imgRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
-        imgRequest.onload = addImage;
-        imgRequest.setRequestHeader('Authorization', 'Client-ID 141d3d9618be5f6fa1e1d32dea90787350f9274a51a9682eda7a2c0e2a570fbd');
+        // var imgRequest = new XMLHttpRequest();
+        // imgRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+        // imgRequest.onload = addImage;
+        // imgRequest.setRequestHeader('Authorization', 'Client-ID 141d3d9618be5f6fa1e1d32dea90787350f9274a51a9682eda7a2c0e2a570fbd');
 
-        imgRequest.send();
+        // imgRequest.send();
+        $.ajax({
+            url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+            headers: {
+                authorization: 'Client-ID 141d3d9618be5f6fa1e1d32dea90787350f9274a51a9682eda7a2c0e2a570fbd'
+            }
+        }).done(addImage);
 
         // 添加文章
-        var articleRequest = new XMLHttpRequest();
-        articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=9ff44f221b134b42a8f25ee15a006df6`);
-        articleRequest.onload = addArticles;
+        // var articleRequest = new XMLHttpRequest();
+        // articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=9ff44f221b134b42a8f25ee15a006df6`);
+        // articleRequest.onload = addArticles;
 
-        articleRequest.send();
+        // articleRequest.send();
+        $.ajax({
+            url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=9ff44f221b134b42a8f25ee15a006df6`
+        }).done(addArticles);
 
     });
 
     // 添加图片
-    function addImage() {
+    function addImage(data) {
         let htmlContent = '';
         // 获取JSON数据
-        const data = JSON.parse(this.responseText);
+        // const data = JSON.parse(this.responseText);
 
         if (data && data.results && data.results[0]) {
             const firstImg = data.results[0];
@@ -44,9 +53,9 @@
         responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
     }
 
-    function addArticles() {
+    function addArticles(data) {
         let htmlContent = '';
-        const data = JSON.parse(this.responseText);
+        // const data = JSON.parse(this.responseText);
 
         if (data.response && data.response.docs && data.response.docs.length > 1) {
             htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article">
