91d27d66ba72a5e89b5684e7a77111c84040945d
xiaofan <1353815633@qq.com>
2018-05-17 22:41:58 +0800

    request-Fetch

--- async-w-Fetch/app.js
+++ async-w-Fetch/app.js
@@ -9,28 +9,25 @@
         responseContainer.innerHTML = '';
         searchedForText = searchField.value;
         // 添加图片
-        // var imgRequest = new XMLHttpRequest();
-        // imgRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
-        // imgRequest.onload = addImage;
-        // imgRequest.setRequestHeader('Authorization', 'Client-ID 141d3d9618be5f6fa1e1d32dea90787350f9274a51a9682eda7a2c0e2a570fbd');
 
-        // imgRequest.send();
-        $.ajax({
-            url: `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`,
+        fetch(`https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`, {
             headers: {
                 authorization: 'Client-ID 141d3d9618be5f6fa1e1d32dea90787350f9274a51a9682eda7a2c0e2a570fbd'
             }
-        }).done(addImage);
+        })
+            .then(response => {
+                return response.json();
+            })
+            .then(addImage)
+            .catch(error => requestError(error, 'image'));
 
         // 添加文章
-        // var articleRequest = new XMLHttpRequest();
-        // articleRequest.open('GET', `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=9ff44f221b134b42a8f25ee15a006df6`);
-        // articleRequest.onload = addArticles;
-
-        // articleRequest.send();
-        $.ajax({
-            url: `http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=9ff44f221b134b42a8f25ee15a006df6`
-        }).done(addArticles);
+        fetch(`http://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchedForText}&api-key=9ff44f221b134b42a8f25ee15a006df6`)
+            .then(response => {
+                return response.json();
+            })
+            .then(addArticles)
+            .catch(error => requestError(error, 'articles'));
 
     });
 
@@ -38,10 +35,9 @@
     function addImage(data) {
         let htmlContent = '';
         // 获取JSON数据
-        // const data = JSON.parse(this.responseText);
+        const firstImg = data.results[0];
 
-        if (data && data.results && data.results[0]) {
-            const firstImg = data.results[0];
+        if (firstImg) {
             htmlContent = `<figure>
                 <img src="${firstImg.urls.regular} alt="${searchedForText}">
                 <figcapiton>${searchedForText} by ${firstImg.user.name}</figcapiton>
@@ -55,10 +51,10 @@
 
     function addArticles(data) {
         let htmlContent = '';
-        // const data = JSON.parse(this.responseText);
 
         if (data.response && data.response.docs && data.response.docs.length > 1) {
-            htmlContent = '<ul>' + data.response.docs.map(article => `<li class="article">
+            const articles = data.response.docs;
+            htmlContent = '<ul>' + articles.map(article => `<li class="article">
                 <h2><a href="${article.web_url}">${article.headline.main}</a></h2>
                 <p>${article.snippet}</p>
             </li>`).join('') + '</ul>';
@@ -66,6 +62,11 @@
             htmlContent = `<div class="error-no-articles">No Articles To Show</div>`;
         }
         responseContainer.insertAdjacentHTML('beforeend', htmlContent);
+    }
+
+    function requestError(error, part) {
+        console.log(error);
+        responseContainer.insertAdjacentHTML('beforeend', `<p class="network-waring">Oh no! There was an error making a request for the ${part}.</p>`);
     }
 
 })();
